560aab22f25634438794c1ffcc3f44e7113a2d35
Eric Paysant <eric.paysant@outlook.fr>
2026-04-10 08:50:05 +0200

    Fix URL Google Apps Script

--- script.js
+++ script.js
@@ -239,7 +239,7 @@ contactForm.addEventListener('submit', (e) => {
 // ============================================
 // RATING WIDGET
 // ============================================
-const RATING_API = 'https://script.google.com/macros/s/AKfycbxllurw2aotR-KQ3a9v1mHsS7XrfK-B6h7Y_4-8oKzvKCTXN-vB_ocxIt8fZpNLX1/exec';
+const RATING_API = 'https://script.google.com/macros/s/AKfycbxIlurw2aotR-KQ3a9v1mHsS7XrfK-B6h7Y_4-8oKzvKCTXN-vB_ocxIt8fZpNLX18/exec';
 
 // Load average on page load
 function loadRating() {
